dcedf9437e72c28b985461db5512b1e31e81e9df
mengwenhao <xmisbest@163.com>
2021-08-26 16:57:23 +0800

    new moudle  --- xm ~~~~~~~~~~~~~~~ is coming!!!!!!!

---
+++ xm/src/main/java/com/brzm/XmApplication.java
@@ -0,0 +1,15 @@
+package com.brzm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class XmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XmApplication.class,args);
+    }
+}
---
+++ xm/src/main/java/com/brzm/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.brzm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/a")
+    public String a(){
+        return "hello world!";
+    }
+}
